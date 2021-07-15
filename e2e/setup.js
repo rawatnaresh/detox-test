713af7ea7aea9c07f21973726f06169679fad17d
Naresh Rawat <nareshrawat33@gmail.com>
2021-07-15 12:48:15 +0545

    remove

--- e2e/setup.js
+++ e2e/setup.js
@@ -2,6 +2,5 @@
 jest.setTimeout(120000);
 
 beforeAll(async () => {
-  await device.launchApp({permissions: {location: 'inuse', photos: 'YES'}});
-  await device.reloadReactNative();
+  await device.launchApp();
 });
